2ed1e3259fb67849c95e540272fdb36640a41520
Matheus Freitas <msformigteen@live.com>
2023-06-11 13:15:35 -0300

    refact: remove not used variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Procedural from "./Versions/Procedural";
+//import Procedural from "./Versions/Procedural";
 import Funcional from "./Versions/Funcional";
 
 function App() {
